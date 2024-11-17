bdee8c024d998a07dca3228a8b70cb9679e12f9a
Mohammed <avq7yu@virginia.edu>
2024-11-16 20:59:03 -0500

    minor edits

--- Untested docs/velocity.py
+++ Untested docs/velocity.py
@@ -5,3 +5,5 @@ r = stretch_body.robot.Robot() #create an instance of the robot object
 r.end_of_arm.get_joint('wrist_yaw').set_velocity(0.1) # rotate CCW at 0.1 rad/s
 #for the base of the robot, the set_velocity aapi can take to parameters
 r.base.set_velocity(0.01, 0.05) # follow a circular path
+
+r.arm.trajectory.add(t_s=20.0, x_m=0.0, v_m=0.0) # 20 second trajectory extending & retracting the arm
